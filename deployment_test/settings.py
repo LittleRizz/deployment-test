5c2c5cf9c93a17d3bf1a92cb072f61e41eecc05c
Drew Ruselowski <druselow@gmail.com>
2015-05-28 12:01:07 -0700

    messed around with database pointers in settings.py

--- deployment_test/settings.py
+++ deployment_test/settings.py
@@ -77,14 +77,25 @@ WSGI_APPLICATION = 'deployment_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'deployment_db',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+         'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': os.environ['RDS_DB_NAME'],
+         'USER': os.environ['RDS_USERNAME'],
+         'PASSWORD': os.environ['RDS_PASSWORD'],
+         'HOST': os.environ['RDS_HOSTNAME'],
+         'PORT': os.environ['RDS_PORT'],
+         }
+     }
+     
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'deployment_db',
+        }
     }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
